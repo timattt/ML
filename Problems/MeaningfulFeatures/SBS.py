45b50f80b7bbb2316b51aa1a41ec2da2450deed4
timattt <timattt@gmail.com>
2023-02-14 14:44:45 +0300

    sbs

---
+++ Problems/MeaningfulFeatures/SBS.py
@@ -0,0 +1,107 @@
+from sklearn.base import clone
+from itertools import combinations
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import pandas as pd
+
+#
+# SBS
+#
+
+class SBS():
+    def __init__(self, estimator, k, scoring=accuracy_score, test_size=0.25, random_state = 1):
+        self.estimator = estimator
+        self.k = k
+        self.scoring = scoring
+        self.test_size = test_size
+        self.random_state = random_state
+        
+    def fit(self, X, y):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=self.test_size,
+                                                            random_state = self.random_state)
+        
+        dim = X_train.shape[1]
+        self.indices_ = tuple(range(dim))
+        self.subsets_ = [self.indices_]
+        
+        score = self._calc_score(X_train, y_train, X_test, y_test, self.indices_)
+        
+        self.scores_ = [score]
+        
+        while dim > self.k:
+            scores = []
+            subsets = []
+            
+            for p in combinations(self.indices_, r=dim-1):
+                score = self._calc_score(X_train, y_train, X_test, y_test, p)
+                
+                scores.append(score)
+                subsets.append(p)
+                
+            best = np.argmax(scores)
+            self.indices_ = subsets[best]
+            self.subsets_.append(self.indices_)
+            self.scores_.append(scores[best])
+            dim -= 1
+            
+        self.k_score_ = self.scores_[-1]
+        
+        return self
+        
+    def transform(self, X):
+        return X[:, self.indices_]
+    
+    def _calc_score(self, X_train, y_train, X_test, y_test, indices):
+        self.estimator.fit(X_train[:, indices], y_train)
+        y_pred = self.estimator.predict(X_test[:, indices])
+        score = self.scoring(y_test, y_pred)
+        return score
+    
+#
+# Load data
+#
+
+url='https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data'
+df_wine = pd.read_csv(url, header=None)
+df_wine.columns = ['метка класса', 'алкоголь', 'яблочная кислота', 'зола',
+                   'щелочность золы', 'магний', 'всего фенола', 'флаваноиды',
+                   'фенолы нефлаваноидные', 'проантоцианины',
+                   'интенсивность цвета', 'оттенок', 'разбавление', 'пролин']
+
+print(df_wine.head())
+
+#
+# Transform data
+#
+
+from sklearn.model_selection import train_test_split
+
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state = 0)
+
+from sklearn.preprocessing import StandardScaler
+
+stdsc = StandardScaler()
+X_train_std = stdsc.fit_transform(X_train)
+X_test_std = stdsc.transform(X_test)
+
+#
+# Test
+#
+
+from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
+
+knn = KNeighborsClassifier(n_neighbors=2)
+sbs = SBS(knn, k=1)
+sbs.fit(X_train_std, y_train)
+
+k_feat = [len(k) for k in sbs.subsets_]
+plt.plot(k_feat, sbs.scores_, marker='o')
+plt.ylim([0.7, 1.1])
+plt.ylabel('Верность')
+plt.xlabel('Число признаков')
+plt.grid()
+plt.show()
+    
